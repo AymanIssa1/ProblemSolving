c73847d56a542a6503b4b4d022d21e88831d16e9
ayman003 <aymanadel003@gmail.com>
2018-08-26 11:08:27 +0200

    28. Implement strStr()

---
+++ app/src/main/java/ayman/problemsolving/com/ImplementstrStr.java
@@ -0,0 +1,11 @@
+package ayman.problemsolving.com;
+
+public class ImplementstrStr {
+
+    // https://leetcode.com/problems/implement-strstr/description/
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+}
