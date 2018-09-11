0885d805a41d42f1e762ccec0a5d91122eaba1e0
ayman003 <aymanadel003@gmail.com>
2018-09-11 11:39:44 +0200

    344. Reverse String

--- app/src/main/java/ayman/problemsolving/com/ReverseString.java
+++ app/src/main/java/ayman/problemsolving/com/ReverseString.java
@@ -10,17 +10,15 @@ public class ReverseString {
 
     public static String reverseString(String s) {
         int endCursor = s.length()-1;
+        char[] sChars = s.toCharArray();
         for (int i = 0; i < s.length()/2; i++){
-            char temp = s.charAt(i);
-            s = replaceCharAt(s,i,s.charAt(endCursor));
-            s = replaceCharAt(s,endCursor,temp);
+            char temp = sChars[i];
+            sChars[i] = sChars[endCursor];
+            sChars[endCursor] = temp;
             endCursor--;
         }
-        return s;
+        return String.valueOf(sChars);
     }
 
-    public static String replaceCharAt(String s, int pos, char c) {
-        return s.substring(0, pos) + c + s.substring(pos + 1);
-    }
 
 }
